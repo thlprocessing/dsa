a20d063ac675bd2e877378d1d4e80a5af30714ac
tannguyen <tannguyenfns@gmail.com>
2026-04-08 02:14:25 -0600

    Binary Search: - PHP Version

--- 5-binary-search/PHP/BinarySearch.php
+++ 5-binary-search/PHP/BinarySearch.php
@@ -37,6 +37,57 @@ class BinarySearch {
         # target is not in array
         return -1;
     }
+
+    public function lowerBound($numbs, $target) {
+
+        # initialize 2 pointers: low, high
+        $low = 0;
+        $high = count($numbs) - 1;
+        $leftMost = count($numbs);
+
+        # Iterate and Compare
+        while ($low <= $high) {
+
+            # Recalculate $mid for each new boundary
+            $mid = floor($low + ($high - $low) / 2);        # Round down: 0.99 ~ 0.00
+
+            # Divide
+            if (isset($numbs[$mid]) && $target <= $numbs[$mid]) {
+                $leftMost = $mid;
+                $high = $mid -1;            # search the left half => discard Half Right (high = mid - 1)
+            } else {
+                $low = $mid + 1;            # search the right half => discard Half Left (low = mid + 1)
+            }
+        }
+
+        return $leftMost;
+    }
+
+
+    public function upperBound($numbs, $target) {
+
+        # initialize 2 pointers: low, high
+        $low = 0;
+        $high = count($numbs) - 1;
+        $rightMost = count($numbs);
+
+        # Iterate and Compare
+        while ($low <= $high) {
+
+            # Recalculate $mid for each new boundary
+            $mid = floor($low + ($high - $low) / 2);        # Round down: 0.99 ~ 0.00
+
+            # Divide
+            if (isset($numbs[$mid]) && $target < $numbs[$mid]) {
+                $rightMost = $mid;
+                $high = $mid -1;            # search the left half => discard Half Right (high = mid - 1)
+            } else {
+                $low = $mid + 1;            # search the right half => discard Half Left (low = mid + 1)
+            }
+        }
+
+        return $rightMost;
+    }
 }
 
 
@@ -56,4 +107,16 @@ echo $solution->search([-1,0,3,5,9,12], 9) . "\n";
  * Output: -1
  * Explanation: 2 does not exist in nums so return -1
  */
-echo $solution->search([-1,0,3,5,9,12], 2);
+echo $solution->search([-1,0,3,5,9,12], 2) . "\n";
+
+echo "LowerBound: Left-Boundary \n";
+echo $solution->lowerBound([1, 2, 4, 4, 7], 4) . "\n";      # Output: 2
+echo $solution->lowerBound([1, 2, 4, 4, 7], 3) . "\n";      # Output: 2
+echo $solution->lowerBound([1, 2, 4, 4, 7], 0) . "\n";      # Output: 0
+echo $solution->lowerBound([1, 2, 4, 4, 7], 8) . "\n";      # Output: 5
+
+echo "UpperBound: Right-Boundary \n";
+echo $solution->upperBound([1, 2, 4, 4, 7], 4) . "\n";      # Output: 4
+echo $solution->upperBound([1, 2, 4, 4, 7], 3) . "\n";      # Output: 2
+echo $solution->upperBound([1, 2, 4, 4, 7], 0) . "\n";      # Output: 0
+echo $solution->upperBound([1, 2, 4, 4, 7], 8) . "\n";      # Output: 5
