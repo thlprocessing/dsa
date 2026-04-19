9a14061d71c916117c5e0788f1f0391e457c3c85
t-nguyen-fns <tannguyenfns@gmail.com>
2026-04-18 19:30:40 -0600

    Counting Sort: Stable Sort

--- 6-sort/PHP/CountingSort.php
+++ 6-sort/PHP/CountingSort.php
@@ -7,28 +7,46 @@ class CountingSort {
         
         $countingFreq = [];
 
-        // php array maintain insertion order for dynamic keys encountered
+        # php array maintain insertion order for dynamic keys encountered
         $maxValue = max($numbs);
-        // initialize count array with 0s to force correct order of indexes
+        # initialize count array with 0s to force correct order of indexes
         $countingFreq = array_fill(0, $maxValue + 1, 0);
 
-        // counting frequencies
+        # counting frequencies
         foreach($numbs as $numb) {
             $countingFreq[$numb]++;
         }
         
-        $reindex = 0;
-        foreach($countingFreq as $numb => $freq) {
-            for($i = 0; $i < $freq; $i++) {
-                $numbs[$reindex++] = $numb;
-            }
+        // unstable
+        // $reindex = 0;
+        // foreach($countingFreq as $numb => $freq) {
+        //     for($i = 0; $i < $freq; $i++) {
+        //         $numbs[$reindex++] = $numb;
+        //     }
+        // }
+
+        # Step 2: Cumulative cnt array
+        for($i = 1; $i < ($maxValue + 1); $i++) {
+            $countingFreq[$i] += $countingFreq[$i - 1];
         }
+
+        # Step 3: Put numbers in the correct order with stability
+        $ans = array_fill(0, count($numbs), 0);
+
+        foreach (array_reverse($numbs) as $val) {
+            $countingFreq[$val] -= 1;
+            $ans[$countingFreq[$val]] = $val;
+        }
+        
+
+        return $ans;
+
     }
 
 }
 
 $data = [1, 2, 3, 0, 6, 0, 1, 1, 3];
 $solution = new CountingSort();
-$solution->countingSort($data);
+$result = $solution->countingSort($data);
 
-echo "counting sort: " . implode(",", $data) . "\n";
+echo "counting sort: " . implode(",", $result) . "\n";
